bd42b1341fd9699153a2b940c9dd54169680f208
Firoz <firozshaik715@gmail.com>
2025-08-19 09:58:20 +0530

    Customized UI with NavBar,Footer and basic resume templates

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,38 @@
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumAi" },
+    { name: "description", content: "Smart Feedback for your Resume" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover" >
+    <Navbar />
+    <section className="main-section" >
+      <div className="page-heading py-16" >
+        <h1>Track Your Applications & Resume Rating</h1>
+        <h2>
+          Smart Feedback for your Resume
+        </h2>
+      </div>
+    </section>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section" >
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))
+        }
+      </div>
+
+    )}
+
+        <Footer/>
+  </main>
+  
 }
